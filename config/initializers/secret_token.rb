932f9df7c4a17636a7b1bda7fc8b0a3cd9d07c82
nickborromeo <ngborromeo@gmail.com>
2012-08-22 14:57:31 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SanctuaryReport::Application.config.secret_token = 'a977981d0485436cd7368fe981540a3d97362a95d11452d1fe8753efc363bace37edd16b1d60036a73a2f0c70e0dcfaab6f6eda830b097baeff967efd73aece6'
