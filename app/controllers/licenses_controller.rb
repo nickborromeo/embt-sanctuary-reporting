c497eb0f2e45ec810a28fd2dcf8cfc0252b399c8
nickborromeo <ngborromeo@gmail.com>
2012-11-21 13:49:04 -0800

    Change the respond_to format so that the downloaded file will contain data

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -40,7 +40,7 @@ class LicensesController < ApplicationController
     @export = ActiveRecord::Base.connection.select_rows("Select DISTINCT CONCAT(C.First_Name, CONCAT(' ', C.Last_Name)),  ssb.serial_number, C.E_Mail, con.COMPANY, slp.TITLE, sp.DESCRIPTION, spv.PRODUCT_VERSION, oi.PID, C.Country, sli.created from WEBMGR.TABLE_CONTACT c, WEBMGR.SANCT_SERIALNO_BANK ssb, WEBMGR.SANCT_LICENSE_ISSUANCE sli, WEBMGR.SANCT_LICENSE_PACKAGE slp,  SANCTUARY.PART_INSTANCE_SERIAL pis, SANCTUARY.ORDER_ITEM oi, SANCTUARY.ORDER_CONTACT oc, SANCTUARY.CONTACT con, WEBMGR.SANCT_PRODUCT sp, WEBMGR.SANCT_PRODUCT_VERSION spv, WEBMGR.SANCT_PRODUCT_SKU spsku, WEBMGR.SANCT_LICENSE_CONFIG slc where (c.USERID = sli.USER_ID) and (ssb.SERIAL_ID = sli.SERIAL_ID) and (ssb.SERIAL_ID = pis.SERIAL_ID) and (slp.PACKAGE_ID = ssb.PACKAGE_ID) and (pis.INSTANCE_ID = oi.INSTANCE_ID) and (oi.ORDER_ID = oc.ORDER_ID) And (Oc.Contact_Id = Con.Contact_Id) AND (sp.PRODUCT_ID = spv.PRODUCT_ID) AND (spv.VERSION_ID = spsku.VERSION_ID) AND (spsku.VERSION_ID  = slc.VERSION_ID) AND (slc.CONFIG_ID = slp.HOST_LICENSE_CFG) AND (c.upper_company_name IS NULL OR c.upper_company_name not like 'EMBARCADERO%') AND c.upper_e_mail not like '%@CODEGEAR.COM'And C.Upper_E_Mail Not Like '%@EMBARCADERO.COM'AND con.upper_company not like 'EMBARCADERO%' AND con.upper_email not like '%@CODEGEAR.COM'AND con.upper_email not like '%@EMBARCADERO.COM'and c.employee = 'F' and con.employee = 'F' and slp.company_id = 1 "+$prod_family+" AND con.COMPANY LIKE '%"+$company+"%' And Sli.Created Between To_Date ('"+$sDate+"', 'mm/dd/yyyy') And To_Date ('"+$eDate+"', 'mm/dd/yyyy')")
     
       respond_to do |format|
-        format.xls
+        format.xls {@export.to_xls}
       end
   end
   
